3b1e576c12e3d601ab1ed1f5b6bc8a3025750d0b
Ika <rexar.ika@gmail.com>
2024-08-14 00:16:23 +0300

    memoization

--- fibonacci/fibonacci/src/Fibonacci.java
+++ fibonacci/fibonacci/src/Fibonacci.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibEffective(100));
+        int n = 100;
+        long[] mem = new long[n + 1];
+        Arrays.fill(mem, -1);
+        System.out.println(fibNaive(n, mem));
     }
     //наивный, медленный, очевидный
-//    private static long fibNaive(int n) {
-//        if(n <= 1)
-//            return n;
-//        return fibNaive(n - 1) + fibNaive(n - 2);
-//    }
+    private static long fibNaive(int n, long[] mem) {
+        if (mem[n] != -1)
+            return mem[n];
+        if(n <= 1)
+            return n;
+        long result = fibNaive(n - 1, mem) + fibNaive(n - 2, mem);
+        mem[n] = result;
+
+        return result;
+    }
     private static long fibEffective(int n) {
         long[] arr = new long[n + 1];
 
