db5a7765d825762209f1228afcd2e9cfa7272494
Ika <rexar.ika@gmail.com>
2024-08-15 01:31:08 +0300

    lazy_algorithm_final

--- greedy/src/greedNum.java
+++ greedy/src/greedNum.java
@@ -9,7 +9,9 @@ public class greedNum {
         System.out.println(greedNumbers(123));
         int[] digits = {3, 1, 7, 9, 9, 5};
         System.out.println(maxNumberFromDigits(digits));
-        
+
+        int[] stations = {0, 200, 375, 550, 950};
+        System.out.println(minStops(stations, 400));
     }
 public static int greedNumbers(int numbers) {
     ArrayList<Integer> nums = new ArrayList<>();
@@ -39,5 +41,26 @@ public static String maxNumberFromDigits(int[] digits) {
         }
         return result;
 }
+
+public static int minStops(int[] stations, int capacity) {
+        int result = 0;
+        int currentStop = 0;
+
+        while (currentStop < stations.length - 1) {
+            int nextStop = currentStop;
+            while (nextStop < stations.length - 1 && stations[nextStop + 1] - stations[currentStop] <= capacity) {
+                nextStop++;
+            }
+            if(currentStop == nextStop) return -1;
+
+            if (nextStop < stations.length - 1) {
+                result++;
+            }
+            currentStop = nextStop;
+        }
+        return result;
 }
+
+}
+
 
